1c5727c54cfe5df8d26a8e15ad6eff4f69138dba
MrLerich <93153582+MrLerich@users.noreply.github.com>
2023-06-03 12:41:38 +0300

    add geopy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
+from geopy.geocoders import Nominatim
 
 city: str = input('Название города: ').capitalize()
+
 url: str = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
 
 weather_data = requests.get(url).json()
@@ -8,6 +10,19 @@ weather_data = requests.get(url).json()
 temperature: int = round(weather_data['main']['temp'])
 temperature_feels: int = round(weather_data['main']['feels_like'])
 
+# calling the Nominatim tool
+loc = Nominatim(user_agent="GetLoc")
+# entering the location name
+getLoc = loc.geocode(city)
+
+
+
 if __name__ == "__main__":
+    # printing address
+    print("Местоположение: ", getLoc.address)
+    # printing latitude and longitude
+    print("Широта = ", getLoc.latitude)
+    print("Долгота = ", getLoc.longitude)
     print(f'Сейчас в городе {city} {temperature}°C')
     print(f'Ощущается как {temperature_feels}°C')
+
